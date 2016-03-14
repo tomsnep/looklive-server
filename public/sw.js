97bd7a353c8d37af2cdcf67a93dd9b6c1edd373f
Tom Snepvangers <tomsnep@gmail.com>
2016-03-14 15:18:32 +0100

    added readme img map and service worker file

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+this.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open('look-live-v1').then(cache => {
+			return cache.addAll([
+				'./',
+				'/styles/style.css',
+				'/js/app.js',
+				'/images/logo.png',
+				'/images/spritesheet.png'
+			]);
+		})
+	);
+});
